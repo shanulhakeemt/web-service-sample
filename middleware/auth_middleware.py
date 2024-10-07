ee607c9f26b494cefb5e9223d647a53b3b7fb0c6
shanul hakeem <shanulhakeem60@gmail.com>
2024-10-08 00:58:39 +0530

    .

--- middleware/auth_middleware.py
+++ middleware/auth_middleware.py
@@ -1,12 +1,15 @@
 from fastapi import HTTPException, Header
 import jwt
+import os
 
+# Get the secret key from environment variables
+SECRET_KEY = os.getenv("JWT_SECRET_KEY", "default_secret")
 
 def auth_middleware(x_auth_token=Header()):
     try:
       if not x_auth_token:
             raise HTTPException(401,"No auth token, Access denied!")
-      verified_token=jwt.decode(x_auth_token,"password_key",["HS256"])
+      verified_token=jwt.decode(x_auth_token,SECRET_KEY,["HS256"])
       if not verified_token:
             raise HTTPException(401,"Verification failed, autherization denied!")
             
